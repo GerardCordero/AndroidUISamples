4ba0da58ca5f72605b6dd92c5801a7f3bb6aaf92
Gerard Cordero <gerard@gerards-macbook-pro-2.local>
2014-04-30 20:52:37 -0700

    Took out unused Imports

--- GCUIAccessSampleNoFragment/src/com/example/gcuiaccesssamplenofragment/MainActivity.java
+++ GCUIAccessSampleNoFragment/src/com/example/gcuiaccesssamplenofragment/MainActivity.java
@@ -1,21 +1,11 @@
 package com.example.gcuiaccesssamplenofragment;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
-import android.app.FragmentManager;
-import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.os.Build;
 
 public class MainActivity extends Activity {
 	//Private variables for the EditText and TextView
